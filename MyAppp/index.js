ca71682c09c30192799a71dc8993b6a53d3b6968
Alexandre <alexandre.vittenet@epitech.eu>
2024-09-30 13:08:46 +0200

    Add React Native App

--- MyAppp/index.js
+++ MyAppp/index.js
@@ -1,5 +1,6 @@
 import {AppRegistry} from 'react-native';
 import App from './App.js';
-import {name as appName} from './app.json';
+import appJson from './app.json';
+const appName = appJson.name; // Utilisation de l'export par défaut
 
 AppRegistry.registerComponent(appName, () => App);
--- MyAppp/index.web.js
+++ MyAppp/index.web.js
@@ -1,6 +1,7 @@
 import { AppRegistry } from 'react-native';
 import App from './App.js';
-import { name as appName } from './app.json';
+import appJson from './app.json';
+const appName = appJson.name; // Utilisation de l'export par défaut
 
 // Ajoutez cette ligne pour utiliser react-native-web
 AppRegistry.registerComponent(appName, () => App);
