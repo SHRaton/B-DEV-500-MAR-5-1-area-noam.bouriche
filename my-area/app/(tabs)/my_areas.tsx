a0741f5a83ee01ff0f15e6f11053aed89b8011a7
Alexandre <alexandre.vittenet@epitech.eu>
2024-11-02 19:02:41 +0100

    [Front] --> update of the front on the my_areas page with logos / more specific infos / better visual

--- my-area/app/(tabs)/my_areas.tsx
+++ my-area/app/(tabs)/my_areas.tsx
@@ -9,12 +9,20 @@ interface Area {
   description: string;
   key: string;
   isActive: boolean;
+  action_1?: string;
+  action_1_info?: string;
   reaction_1?: string;
+  reaction_1_info?: string;
   reaction_2?: string;
+  reaction_2_info?: string;
   reaction_3?: string;
+  reaction_3_info?: string;
   reaction_4?: string;
+  reaction_4_info?: string;
   reaction_5?: string;
+  reaction_5_info?: string;
   reaction_6?: string;
+  reaction_6_info?: string;
 }
 
 const MyAreas: React.FC = () => {
@@ -117,14 +125,26 @@ const MyAreas: React.FC = () => {
       <View style={styles.areaInfo}>
         <Text style={styles.areaName}>{item.name}</Text>
         <Text style={styles.areaDescription}>{item.description}</Text>
-        <ScrollView>
-          {[...Array(6)].map((_, index) => (
-            item[`reaction_${index + 1}` as keyof Area] ? (
-              <Text key={index} style={styles.areaReaction}>
-                Reaction {index + 1}: {item[`reaction_${index + 1}` as keyof Area]}
-              </Text>
-            ) : null
-          ))}
+        <View style={styles.actionContainer}>
+          <Image source={getLogoSource(item.action_1)} style={styles.actionLogoLarge} resizeMode="contain"/>
+          <Text style={styles.areaReaction}>Action : {getSummaryText(item.action_1, item.action_1_info)}</Text>
+        </View>
+        <ScrollView style={styles.scrollview}>
+          {[...Array(6)].map((_, index) => {
+            const reactionKey = `reaction_${index + 1}` as keyof Area;
+            const reactionInfoKey = `reaction_${index + 1}_info` as keyof Area;
+            const reactionName = item[reactionKey];
+            const reactionInfo = parseDetails(item[reactionInfoKey] as string | undefined);
+
+            return reactionName ? (
+              <View style={styles.reactionContainer} key={index}>
+                <Image source={getLogoSource(reactionName)} style={styles.actionLogoLarge} resizeMode="contain"/>
+                <Text style={styles.areaReaction}>
+                  Reaction {index + 1}: {getSummaryText(reactionName as string, reactionInfo)}
+                </Text>
+              </View>
+            ) : null;
+          })}
         </ScrollView>
       </View>
       <TouchableOpacity
@@ -148,6 +168,90 @@ const MyAreas: React.FC = () => {
       />
     </Pressable>
   );
+
+  const parseDetails = (details: string | undefined) => {
+    try {
+      return details ? JSON.parse(details) : {};
+    } catch (error) {
+      console.error("Error parsing details:", error);
+      return {};
+    }
+  };
+
+  const getLogoSource = (logoName: string | undefined) => {
+    switch (logoName) {
+      case 'Deepl_1':
+        return require('../../assets/logos/deepl.png');
+      case 'Discord_1':
+        return require('../../assets/logos/discord.png');
+      case 'Discord_2':
+        return require('../../assets/logos/discord.png');
+      case 'Spotify_1':
+        return require('../../assets/logos/spotify.png');
+      case 'Spotify_2':
+        return require('../../assets/logos/spotify.png');
+      case 'Spotify_3':
+        return require('../../assets/logos/spotify.png');
+      case 'Spotify_4':
+        return require('../../assets/logos/spotify.png');
+      case 'Spotify_5':
+        return require('../../assets/logos/spotify.png');
+      case 'Spotify_6':
+        return require('../../assets/logos/spotify.png');
+      case 'Telegram_1':
+        return require('../../assets/logos/telegram.png');
+      case 'BTC':
+        return require('../../assets/logos/gecko.png');
+      case 'IS_STREAMING':
+        return require('../../assets/logos/twitch.png');
+      case 'DETECT_MESSAGE':
+        return require('../../assets/logos/twitch.png');
+      case 'IS_RAINING':
+        return require('../../assets/logos/weather.png');
+      case 'IS_NIGHT':
+        return require('../../assets/logos/weather.png');
+      case 'none':
+      default:
+        return require('../../assets/logos/none.png');
+    }
+  };
+
+  const getSummaryText = (actionName: string | undefined, details: { [key: string]: string | "test"}) => {
+    console.log(actionName);
+    console.log(details);
+    switch (actionName) {
+      case 'Deepl_1':
+        return `Traduit le texte "${details.message}" en ${details.langue}.`;
+      case 'Discord_1':
+        return `Envoie un message privé (${details.message}) à ${details.person}.`;
+      case 'Discord_2':
+        return `Envoie un message dans le canal défini avec le contenu (${details.message}).`;
+      case 'Spotify_1':
+        return `Récupère la playlist de l'utilisateur.`;
+      case 'Spotify_2':
+        return `Récupère les 5 titres préférés de l'utilisateur}.`;
+      case 'Spotify_3':
+        return `Recommande 10 chansons similaires à "${details.song}".`;
+      case 'Spotify_4':
+        return `Recommande 10 artistes similaires à "${details.artist}".`;
+      case 'Spotify_5':
+        return `Récupère les dernières sorties musicales de l'utilisateur ${details.user}.`;
+      case 'Telegram_1':
+        return `Envoie un message (${details.message}) à ${details.person} sur Telegram.`;
+      case 'BTC':
+        return `Vérifie si le prix du Bitcoin a augmenté.`;
+      case 'IS_STREAMING':
+        return `Vérifie si le streamer ${details.streamer} est en live.`;
+      case 'DETECT_MESSAGE':
+        return `Détecte un nouveau message de ${details.person} sur Twitch.`;
+      case 'IS_RAINING':
+        return `Vérifie si il pleut.`;
+      case 'IS_NIGHT':
+        return `Vérifie si il fait nuit.`;
+      default:
+        return `Aucune action spécifiée pour ${actionName}.`;
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -304,6 +408,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#f9f9f9',
     padding: 15,
     borderRadius: 10,
+    borderWidth: 1,
+    borderColor: 'black',
     marginBottom: 20,
     flexDirection: 'row',
     alignItems: 'center',
@@ -322,16 +428,52 @@ const styles = StyleSheet.create({
     color: '#666',
     marginBottom: 10,
   },
-  areaReaction: {
-    fontSize: 14,
-    color: '#333',
-    marginBottom: 5,
-  },
   switch: {
     color: '#2545',
     transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }],  // Agrandir le switch
     alignSelf: 'center',  // Centre verticalement le switch
     marginRight: 50,
+  },
+  areaReaction: {
+    fontSize: 17,
+    color: '#333',
+  },
+  actionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center', // Centers text and image vertically
+    marginBottom: 20,
+    backgroundColor: '#d3d3d3', // Light gray background
+    marginRight: 50,
+    borderRadius: 10,
+    padding: 12,
+    shadowColor: '#000', // Shadow for subtle depth
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 1, height: 2 },
+    shadowRadius: 4,
+  },
+  reactionContainer: {
+    flexDirection: 'row',
+    alignItems: 'center', // Centers text and image vertically
+    marginVertical: 8,
+    paddingVertical: 5,
+    paddingHorizontal: 8,
+    borderRadius: 8,
+  },
+  actionLogoLarge: {
+    width: 35,
+    height: 35,
+    marginRight: 15,
+  },
+  scrollview: {
+    padding: 12,
+    marginRight: 50,
+    borderRadius: 10,
+    backgroundColor: '#f5f3e7', // Soft, raccoon-inspired beige
+    shadowColor: '#000',
+    shadowOpacity: 0.15,
+    shadowOffset: { width: 1, height: 3 },
+    shadowRadius: 4,
+    backgroundImage: 'linear-gradient(45deg, #e8e6e1, #d5d2cd)',
   },
 });
 
